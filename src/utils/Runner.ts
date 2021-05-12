4fd80ae9139a3288d024adcdca9cb45397279dc4
yariel <info@wisegar.org>
2021-05-12 10:33:36 +0200

    Update ObjExt export

--- src/utils/Runner.ts
+++ src/utils/Runner.ts
@@ -1,31 +1,31 @@
-const spawn = require("child_process").spawn;
+const spawn = require('child_process').spawn;
 
-export const executePS = (scriptPath) => {
-  const child = spawn("powershell.exe", [scriptPath]);
+export const executePS = (scriptPath: string) => {
+  const child = spawn('powershell.exe', [scriptPath]);
 
-  child.stdout.on("data", function (data) {
-    console.log("PS: " + data);
+  child.stdout.on('data', function (data: any) {
+    console.log('PS: ' + data);
   });
-  child.stderr.on("data", function (data) {
-    console.log("PS Err: " + data);
+  child.stderr.on('data', function (data: any) {
+    console.log('PS Err: ' + data);
   });
-  child.on("exit", function () {
-    console.log("PS Exit");
+  child.on('exit', function () {
+    console.log('PS Exit');
   });
   child.stdin.end();
 };
 
-export const executeCMD = (scriptPath) => {
-  const child = spawn("cmd.exe", [scriptPath]);
+export const executeCMD = (scriptPath: string) => {
+  const child = spawn('cmd.exe', [scriptPath]);
 
-  child.stdout.on("data", function (data) {
-    console.log("CMD: " + data);
+  child.stdout.on('data', function (data: any) {
+    console.log('CMD: ' + data);
   });
-  child.stderr.on("data", function (data) {
-    console.log("CMD Err: " + data);
+  child.stderr.on('data', function (data: any) {
+    console.log('CMD Err: ' + data);
   });
-  child.on("exit", function () {
-    console.log("CMD Exit");
+  child.on('exit', function () {
+    console.log('CMD Exit');
   });
   child.stdin.end();
 };
